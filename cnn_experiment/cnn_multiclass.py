softmax function fix

cnn_experiment/cnn_multiclass.py
@@ -67,7 +67,7 @@ class CNNClassifier(nn.Module):
         x = self.embeddings(x).transpose(1, 2)  # Conv1d takes (batch, channel, seq_len)
         x = [self.conv_global_max_pool(x, conv) for conv in self.convs]
         x = torch.cat(x, dim=1)
-        x = nn.Softmax(self.fc(x))
+        x = F.softmax(self.fc(x))
 
         return x
 

cnn_experiment/cnn_multiclass_2.py
@@ -67,7 +67,7 @@ class CNNClassifier(nn.Module):
         x = [self.conv_global_max_pool(x, conv) for conv in self.convs]
         x = torch.cat(x, dim=1)
         x = F.relu(self.fc(x))
-        x = nn.Softmax(self.output(x))
+        x = F.softmax(self.output(x))
         return x
 
 if __name__ == "__main__":